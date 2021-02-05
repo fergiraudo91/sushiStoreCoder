dcc5bc12edc8daeb84dc3706482a4130a9535596
Fernando Giraudo <fergiraudo91@gmail.com>
2021-02-05 12:03:10 -0300

    First commit

---
+++ data/sushiData.js
@@ -0,0 +1,404 @@
+const sushiData = [
+    {
+        name: 'Bomba Afrodita',
+        description: 'Salmon, mango, palta y queso philadelphia envuelto en tamago, bañado en salsa de maracuya, coronado con batatas fritas.',
+        img: 'fotos/piezas/bombra_afrodita.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 3,
+                precio: 335,
+            },
+            {
+                unidades: 6,
+                precio: 670
+            }
+        ]
+    },
+    {
+        name: 'Geishas de Salmón',
+        description: 'Geishas de Salmón rellenas de queso Philadelphia con Palta',
+        img: 'fotos/piezas/geishas_salmon.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 3,
+                precio: 245
+            },
+            {
+                unidades: 6,
+                precio: 490
+            }
+        ]
+    },
+    {
+        name: 'HOT Bologna',
+        description: 'Roll rebozado relleno de langostinos salteado con pimentón y philadelphia.',
+        img: 'fotos/piezas/hot_bologna.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 270,
+            },
+            {
+                unidades: 10,
+                precio: 540
+            }
+        ]
+    },
+    {
+        name: 'HOT Firenze',
+        description: 'Roll rebozado relleno de salmon grill, coronado con mayo palta.',
+        img: 'fotos/piezas/hot_firenze.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'HOT New York Philadelphia',
+        description: 'Roll rebozado relleno de salmón, arroz, palta y queso Philadelphia.',
+        img: 'fotos/piezas/hot_new_york_phila.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Nigiri Salmon',
+        description: 'Bocaditos de arroz envuelto en finos cortes del mas fresco salmon.',
+        img: 'fotos/piezas/nigiri_salmon.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 345,
+            },
+            {
+                unidades: 10,
+                precio: 690
+            }
+        ]
+    },
+    {
+        name: 'Roll Apaltado',
+        description: 'Roll relleno de langostinos rebozados queso philadelphia, cubierto con palta.',
+        img: 'fotos/piezas/roll_apaltado.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Roll Bs As',
+        description: 'Roll relleno de queso philadelphia y langostinos rebozados cubierto con una fina capa de salmón.',
+        img: 'fotos/piezas/roll_bs_as.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Roll Calafate',
+        description: 'Roll de tamago relleno de arroz, philadelphia y palta envuelto en salmon rosado.',
+        img: 'fotos/piezas/roll_calafate.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 345,
+            },
+            {
+                unidades: 10,
+                precio: 690
+            }
+        ]
+    },
+    {
+        name: 'Roll California',
+        description: 'Roll relleno de kanikama con philadelphia y palta.',
+        img: 'fotos/piezas/roll_california.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Roll Chop Suey',
+        description: 'Roll relleno con vegetales salteados al wok y palta.',
+        img: 'fotos/piezas/roll_chop_suey.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Roll Clerck',
+        description: 'Roll relleno de salmón cocido con miel y mostaza, queso philadelphia e hilos de batata frita por fuera.',
+        img: 'fotos/piezas/roll_clerck.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Roll New York con Philadelphia',
+        description: 'Piezas de roll relleno de salmón, palta, y queso Philadelphia.',
+        img: 'fotos/piezas/roll_new_york_phila.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Roll Niji',
+        description: 'Salmon, repollo colorado encurtido, palta, envuelto en tamago.',
+        img: 'fotos/piezas/roll_niji.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 245,
+            },
+            {
+                unidades: 10,
+                precio: 490
+            }
+        ]
+    },
+    {
+        name: 'Roll Persia',
+        description: 'Roll relleno de salmon, philadelphia y verdeo cubierto con sésamo tostado.',
+        img: 'fotos/piezas/roll_persia.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Roll Philadelphia',
+        description: 'Roll relleno de salmón y queso philadelphia, cubierto con sésamo tostado.',
+        img: 'fotos/piezas/roll_phila.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Roll Spicy Tuna c/palta',
+        description: 'Roll relleno de atún, mayonesa, verdeo y togarashi.',
+        img: 'fotos/piezas/roll_spicy.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Roll Tora',
+        description: 'Piezas de Roll relleno de langostinos rebozados con palta cubierto en ciboulette y batatas fritas.',
+        img: 'fotos/piezas/roll_tora.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 260,
+            },
+            {
+                unidades: 10,
+                precio: 520
+            }
+        ]
+    },
+    {
+        name: 'Roll Valencia',
+        description: 'Roll relleno de tomates secos, rucula fresca, cebolla caramelizada, philadelphia saborizado con cebolla y ciboulette, cubierto con ciboulette picado.',
+        img: 'fotos/piezas/roll_valencia.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 225,
+            },
+            {
+                unidades: 10,
+                precio: 450
+            }
+        ]
+    },
+    {
+        name: 'Sashimi Salmon',
+        description: 'Selectos cortes del mas fresco salmon.',
+        img: 'fotos/piezas/sashimi_salmon.jpg',
+        type: 'pieza',
+        prices: [
+            {
+                unidades: 5,
+                precio: 345,
+            },
+            {
+                unidades: 10,
+                precio: 690
+            }
+        ]
+    },
+    {
+        name: 'ALASKA',
+        description: 'El clásico de POP! Mucho salmón y algo más.',
+        img: 'fotos/combos/alaska.jpg',
+        type: 'combo',
+        prices: [
+            {
+                unidades: 12,
+                precio: 699,
+            },
+            {
+                unidades: 25,
+                precio: 1429
+            },
+            {
+                unidades: 40,
+                precio: 2129
+            }
+        ]
+    },
+    {
+        name: 'BILBAO',
+        description: 'Riquísimo mix de sabores.',
+        img: 'fotos/combos/bilbao.jpg',
+        type: 'combo',
+        prices: [
+            {
+                unidades: 12,
+                precio: 639,
+            },
+            {
+                unidades: 25,
+                precio: 1289
+            },
+            {
+                unidades: 40,
+                precio: 1999
+            }
+        ]
+    },
+    {
+        name: 'BLACK',
+        description: 'Para darse un gusto completo! Combo premium para probar un poco de todo.',
+        img: 'fotos/combos/bilbao.jpg',
+        type: 'combo',
+        prices: [
+            {
+                unidades: 12,
+                precio: 779,
+            },
+            {
+                unidades: 25,
+                precio: 1559
+            },
+            {
+                unidades: 40,
+                precio: 2399
+            }
+        ]
+    },
+    {
+        name: 'TOKYO',
+        description: 'Comes rico y pagas poco.',
+        img: 'fotos/combos/tokyo.jpg',
+        type: 'combo',
+        prices: [
+            {
+                unidades: 12,
+                precio: 549,
+            },
+            {
+                unidades: 25,
+                precio: 1129
+            },
+            {
+                unidades: 40,
+                precio: 1729
+            }
+        ]
+    }
+];
+
+export default sushiData;
---
+++ js/main.js
@@ -0,0 +1,94 @@
+import sushiData from '../data/sushiData.js';
+console.log(`Nuestro sushi disponible es: `);
+for(let sushi of sushiData){
+    console.log(sushi.name);
+}
+
+class Sushi{
+    constructor(nombre, descripcion, precios, cantidad){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precios = precios;
+        this.cantidad = cantidad;
+    }
+
+    //GETTERS Y SETTERS -> Se utilizan para trabajar el encapsulamiento de una clase 
+    //permiten setear datos de una propiedad y obtenerlos sin necesidad de acceder a la propiedad en si misma
+    getNombre(){
+        return this.nombre;
+    }
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
+    getDescription(){
+        return this.descripcion;
+    }
+    setDescription(description){
+        this.descripcion = description;
+    }
+    getPrecios(){
+        return this.precios;
+    }
+    setPrecios(precios){
+        this.precios = precios;
+    }
+    getCantidad(){
+        return this.cantidad;
+    }
+    setCantidad(cantidad){
+        this.cantidad = cantidad;
+    }
+
+    //Metodos de acciones concretas
+    //El método vender verifica si la cantidad es menor a 0, si lo es dará un mensaje que dirá que no hay más sushi de esa pieza disponible
+    //Si hay unidades disponibles se restará a las que quedan para controlar el stock
+    vender(){
+        if(this.getCantidad() <= 0){
+            alert('No queda más pieza disponible de ese Sushi');
+            return;
+        }
+        //Aquí se restan las unidades que hay disponibles con las ventas realizadas
+        this.setCantidad(this.getCantidad() - 1);
+        alert(`Usted ha comprado exitosamente Sushi ${this.getNombre()}`);
+        alert(`quedan ${this.getCantidad()} unidades`);
+    }
+    //El método reponer suma una cantidad determinada a la cantidad que ya está en la propiedad de la clase
+    reponer(cantidad){
+        this.setCantidad(this.getCantidad() + cantidad);
+        alert(`Usted ahora tiene ${this.getCantidad()} de ${this.getNombre()}`);
+    }
+    
+}
+
+const busqueda = termino =>{
+    return sushiData.find(el => el.name.toLowerCase().includes(termino.toLowerCase()));
+};
+//Uso del método filtro para filtrar por tipo de sushi
+let tipo;
+const filtro = termino => {
+    tipo = termino;
+    return sushiData.filter(el => el.type.includes(termino));
+};
+
+//Utilizar map para aumentar un porcentaje determinado
+const arr = [];
+const aumento = porcentaje => {
+    sushiData.map((el) =>{
+        for(let ind=0; ind<el.prices.length; ind++){
+          arr.push({name: el.name, price: el.prices[ind].precio * (1 + (+porcentaje/100)), unidades: el.prices[ind].unidades});
+        }  
+    }     
+    )};
+
+//buscamos un sushi por prompt
+const {name:sushiName, description, prices} = busqueda(prompt('Ingrese el sushi que quiere encontrar'));
+console.log(`Nombre: ${sushiName}`);
+console.log(`Descripcion: ${description}`);
+console.log(`precios: ${prices[0].unidades} unidades = $${prices[0].precio} - ${prices[1].unidades} unidades = $${prices[1].precio}`);
+//filtramos por tipo
+
+console.log(filtro(prompt('Ingrese el tipo de sushi')), 'SUSHI TIPO ' + tipo.toUpperCase());
+
+//aumentamos el precio a todos los sushi
+aumento(+prompt('ingrese el porcentaje que desea aumentar'));
+console.log(arr);
