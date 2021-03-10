f69df20801943fea773162c67d5a584ecfc046d3
Fernando Giraudo <fergiraudo91@gmail.com>
2021-03-10 17:52:57 -0300

    modificaciones en AJAX

--- js/main.js
+++ js/main.js
@@ -1,140 +1,145 @@
-import sushiData from '../data/sushiData.js';
-const sushiList = $('#sushiList')[0];
+const sushiList = $("#sushiList")[0];
 const carritoArr = [];
-const carritoModal = document.getElementById('display-items');
+const carritoModal = document.getElementById("display-items");
 
+const getDataByJson = async (url) => {
+  const data = await fetch(url);
+  const response = await data.json();
+  return response;
+};
 
-let data = sushiData;
-
-const reenderizar = () =>{
-    data.forEach((el, index) => {
-        const sushi = document.createElement('div');
-        sushi.className = 'sushi-item';
-        let opciones = '';
-        el.prices.forEach(precio => {
-            opciones += `<option value=${precio.unidades}-${precio.precio}>${precio.unidades} unidades - \$${precio.precio}</option>`;
-        });
-        sushi.innerHTML = `
+const reenderizar = async (data) => {
+  data.forEach((el) => {
+    const sushi = document.createElement("div");
+    sushi.className = "sushi-item";
+    let opciones = "";
+    el.prices.forEach((precio) => {
+      opciones += `<option value=${precio.unidades}-${precio.precio}>${precio.unidades} unidades - \$${precio.precio}</option>`;
+    });
+    sushi.innerHTML = `
             <img src='${el.img}' alt='${el.name}' />
             <div class='content'>
             <h2>${el.name}</h2>
             <h4>${el.description}</h4>
-            <select>
+            <select id="selection${el.id}">
                 ${opciones}
             </select>
             <button class='btnCompra'>Agregar Al Carrito</button>
             </div>
         `;
-        sushi.querySelector('button').addEventListener('click', () => {agregarItemCarrito(index)});
-        sushiList.append(sushi);
+    sushiList.append(sushi);
+    const select = document.getElementById(`selection${el.id}`);
+    sushi.querySelector("button").addEventListener("click", () => {
+      agregarItemCarrito(el, select.value);
     });
-}
+  });
+};
 
-
-let id= 0;
-const agregarItemCarrito = (index) => {
-    let prices = document.querySelectorAll('select');
-    let price = prices[index].value.split('-');
-    const sushiItem = data[index];
-    price = {
-        'unidades' : price[0],
-        'precio' : price[1]
-    }
-    let item = {
-        name: sushiItem.name,
-        description: sushiItem.description,
-        img : sushiItem.img,
-        price: price,
-        type: sushiItem.type
-    };
-    localStorage.setItem(id, JSON.stringify(item));
-    id++;
-    carritoArr.push(item);
-    alert(`Usted ha agregado ${data[index].name} al carrito`);
-    
+const agregarItemCarrito = (item, price) => {
+  delete item.prices;
+  price = price.split("-");
+  price = {
+    cantidad: price[0],
+    precio: price[1],
+  };
+  item.price = price;
+  carritoArr.push(item);
+  localStorage.setItem("compra", JSON.stringify(carritoArr));
+  alert(`${item.name} se ha agregado correctamente al carrito`);
 };
 
 const showCarItems = () => {
-    let total = 0;
-    for(let i=0; i<localStorage.length; i++){
-        const item = JSON.parse(localStorage.getItem(i));
-        console.log(item);
-        const {unidades, precio} = item.price;
-        console.log(item)
-        const divItem = document.createElement('div');
-        divItem.className = 'carrito-item';
-        divItem.innerHTML = `<div class="izquierda">
-            <span>${item.name}</span>
-        </div>
-        <div class="derecha">
-            <div class="precio">${unidades} unidades $${precio}</div><span class="trash"><i class="fas fa-trash"></i></span>
-        </div>`;
-        carritoModal.append(divItem);
-        total+= +precio;
-    }
-    const divTotal = document.createElement('div');
-    divTotal.className = "total-container";
-    divTotal.innerHTML = `<span>Total</span> <span>$${total}</span>`;
-    carritoModal.append(divTotal);
-    $('#carrito-modal').css({
-        "display" : "block"
-    })
-    
-    $('#display-items').animate({
-        "width": "40%"
-    }, "slow");
-    $('#close-button').on('click', closeCarrito);
-}
+  let total = 0;
+  const items = JSON.parse(localStorage.getItem("compra")) || [];
+  for (let i = 0; i < items.length; i++) {
+    const { cantidad, precio } = items[i].price;
+    const divItem = document.createElement("div");
+    divItem.className = "carrito-item";
+    divItem.innerHTML = `<div class="izquierda">
+                    <span>${items[i].name}</span>
+                </div>
+                <div class="derecha">
+                    <div class="precio">${cantidad} unidades $${precio}</div><span class="trash"><i class="fas fa-trash"></i></span>
+                </div>`;
+    carritoModal.append(divItem);
+    total += +precio;
+  }
+  const divTotal = document.createElement("div");
+  divTotal.className = "total-container";
+  divTotal.innerHTML = `<span>Total</span> <span>$${total}</span>`;
+  carritoModal.append(divTotal);
+  $("#carrito-modal").css({
+    display: "block",
+  });
 
-const closeCarrito = () =>{
-    $('#display-items').animate({
-        "width": "-40%"
-    }, "slow", () => {
-        $('#carrito-modal').css({
-            "display" : "none"
-        });
-        carritoModal.innerHTML = `
-    <i class="fas fa-window-close close" id="close-button"></i>
-    <h2 class="carrito-id">Tu Pedido</h2>`;
-    });
-    $('#close-button').on('click', closeCarrito);   
-}
+  $("#display-items").animate(
+    {
+      width: "40%",
+    },
+    "slow"
+  );
 
-const limpiarPantalla = () => {
-    sushiList.innerHTML = '';
-}
-
-const filtrarCombo = () => {
-    data = sushiData.filter(el => el.type === 'combo');
-    limpiarPantalla();
-    reenderizar();
+  $("#close-button").on("click", closeCarrito);
 };
 
-const filtrarPieza = () => {
-    data = sushiData.filter(el => el.type === 'pieza');
-    limpiarPantalla();
-    reenderizar();
-}
+const closeCarrito = () => {
+  $("#display-items").animate(
+    {
+      width: "-40%",
+    },
+    "slow",
+    () => {
+      $("#carrito-modal").css({
+        display: "none",
+      });
+      carritoModal.innerHTML = `
+    <i class="fas fa-window-close close" id="close-button"></i>
+    <h2 class="carrito-id">Tu Pedido</h2>`;
+    }
+  );
+  $("#close-button").on("click", closeCarrito);
+};
 
-const filtrarEnsalada = () => {
-    data = sushiData.filter(el => el.type === 'ensalada');
-    limpiarPantalla();
-    reenderizar();
-}
+const limpiarPantalla = () => {
+  sushiList.innerHTML = "";
+};
 
-const filtrarTodo = () => {
-    data = sushiData;
-    limpiarPantalla();
-    reenderizar();
-}
+const filtrarCombo = async () => {
+  const dataFetch = await fetch("data/sushiData.json");
+  const response = await dataFetch.json();
+  const data = response.filter((el) => el.type === "combo");
+  limpiarPantalla();
+  reenderizar(data);
+};
 
-reenderizar();
+const filtrarPieza = async () => {
+  const dataFetch = await fetch("data/sushiData.json");
+  const response = await dataFetch.json();
+  const data = response.filter((el) => el.type === "pieza");
+  limpiarPantalla();
+  reenderizar(data);
+};
 
+const filtrarEnsalada = async () => {
+  const dataFetch = await fetch("data/sushiData.json");
+  const response = await dataFetch.json();
+  const data = response.filter((el) => el.type === "ensalada");
+  limpiarPantalla();
+  reenderizar(data);
+};
 
+const filtrarTodo = async () => {
+  const data = await fetch("data/sushiData.json");
+  const responseData = await data.json();
+  limpiarPantalla();
+  reenderizar(responseData);
+};
 
-$('#carrito').on('click', showCarItems);
-$('#combo').on('click', filtrarCombo);
-$('#pieza').on('click', filtrarPieza);
-$('#ensalada').on('click', filtrarEnsalada);
-$('#todos').on('click', filtrarTodo);
-$('#close-button').on('click', closeCarrito);
+filtrarTodo();
+
+$("#carrito").on("click", showCarItems);
+$("#combo").on("click", filtrarCombo);
+$("#pieza").on("click", filtrarPieza);
+$("#ensalada").on("click", filtrarEnsalada);
+$("#todos").on("click", filtrarTodo);
+$("#close-button").on("click", closeCarrito);
